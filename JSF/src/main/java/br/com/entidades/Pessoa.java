ab5b76722821d14596286ed35f6748d4e8626c9e
LuizHenriqueGomesRibeiro <louisnuuk@hotmail.com>
2023-09-21 18:22:33 -0300

    Revert "comitar primeiro CRUD em Hibernate"
    This reverts commit 5f56af82aac537d756ff37b18cab6ea66c4e487c.

--- JSF/src/main/java/br/com/entidades/Pessoa.java
+++ JSF/src/main/java/br/com/entidades/Pessoa.java
@@ -44,34 +44,43 @@ public class Pessoa implements Serializable {
 	public Pessoa() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getSobrenome() {
 		return sobrenome;
 	}
+
 	public void setSobrenome(String sobrenome) {
 		this.sobrenome = sobrenome;
 	}
+
 	public Integer getIdade() {
 		return idade;
 	}
+
 	public void setIdade(Integer idade) {
 		this.idade = idade;
 	}
+
 	public Date getDataNascimento() {
 		return dataNascimento;
 	}
+
 	public void setDataNascimento(Date dataNascimento) {
 		this.dataNascimento = dataNascimento;
 	}
